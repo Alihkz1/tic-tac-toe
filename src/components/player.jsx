fdc4ef923cbbd8b423bfa041613807b27204518e
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2024-11-03 00:03:42 +0330

    work on player component

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -3,17 +3,17 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
   function edit_onClick() {
-    setIsEditing(!isEditing);
+    setIsEditing((isEditing) => !isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
-  if (isEditing) playerName = <input type="text"></input>;
+  if (isEditing) playerName = <input type="text" value={name}></input>;
 
   return (
     <li>
       {playerName}
       <span className="player-symbol">{symbol}</span>
-      <button onClick={edit_onClick}>Edit</button>
+      <button onClick={edit_onClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
